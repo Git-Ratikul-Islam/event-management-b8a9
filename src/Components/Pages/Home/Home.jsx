5e8a2b14ec492bc484e0fd0a4ff43230c745e639
Ratikulislam <joyofficial80@gmail.com>
2023-10-09 17:42:50 +0600

    new-recent-events-section-created

--- src/Components/Pages/Home/Home.jsx
+++ src/Components/Pages/Home/Home.jsx
@@ -1,5 +1,6 @@
 import { Link, useLoaderData } from "react-router-dom";
 import Cards from "./Cards";
+import RecentEvent from "./RecentEvent/RecentEvent";
 
 
 const Home = () => {
@@ -67,6 +68,36 @@ const Home = () => {
 
 
                   <Cards cards={cards}></Cards>
+                  <div className="bg-[#fcf2eb] overflow-hidden" >
+                        <div className="w-5/6  mx-auto">
+                              <div className="hero lg:min-h-[75vh] min-h-[]" >
+                                    <div className="hero-content flex-col lg:flex-row-reverse">
+                                          <img src="https://i.ibb.co/Rpw0Wqs/wellness-diet-plan-healthy-living-icon.jpg" className="h-[460px] w-[640px] lg:ml-14 object-cover " />
+                                          <div className="lg:ml-24 overflow-x-hidden overflow-y-auto" data-aos="fade-up"
+                                                data-aos-anchor-placement="bottom-bottom">
+                                                <div className="lg:mb-10 mb-4" >
+                                                      <p className="text-[#65c9bb] font-semibold">Upcoming Events</p>
+                                                </div>
+                                                <h1 className="text-5xl font-bold">Health Living</h1>
+                                                <p className="py-6">
+                                                      Healthy living is a holistic approach to well-being, encompassing physical, mental, and emotional health. It involves maintaining a balanced diet, regular exercise, adequate sleep, and stress management. Hydration, mindfulness, and positive social connections also play crucial roles. Prioritizing these aspects can lead to a happier, longer, and more fulfilling life.</p>
+                                                <div className="flex gap-6">
+                                                      <div>
+                                                            <Link to="/events" className=""><button className="btn btn-primary mt-4 text-white rounded-none bg-[#f15b43] border-none hover:bg-[#7ed957] px-10">All Events</button></Link>
+                                                      </div>
+
+                                                      <div>
+                                                            <Link to="/events"><a className="btn mt-4 bg-[#f15b43] text-white rounded-none border-none bg-none hover:bg-[#7ed957] px-10">Services</a> </Link>
+                                                      </div>
+                                                </div>
+                                          </div>
+                                    </div>
+                              </div>
+
+                        </div>
+                  </div>
+                  <RecentEvent></RecentEvent>
+
 
 
 
---
+++ src/Components/Pages/Home/RecentEvent/RecentEvent.jsx
@@ -0,0 +1,193 @@
+
+
+const RecentEvent = () => {
+      return (
+            <section className="pt-20 lg:pt-[120px] pb-10 lg:pb-20 ">
+                  <div className="overflow-hidden">
+                        <div className="flex flex-wrap justify-center -mx-4">
+                              <div className="w-full px-4">
+                                    <div className="text-center mx-auto mb-[60px] lg:mb-20 max-w-[510px]">
+                                          <span className="font-semibold text-lg text-[#65c9bb] mb-2 block">
+                                                Our Events
+                                          </span>
+                                          <h2
+                                                className=" 
+                  font-bold
+                  text-3xl
+                  sm:text-4xl
+                  md:text-[40px]
+                  text-dark
+                  mb-4
+
+                  "
+                                          >
+                                                Our Recent Events
+                                          </h2>
+                                          <p className="text-base text-body-color">
+                                                There are some of our recent events which have been celebrated successfully with joyful people all over the world.
+                                          </p>
+                                    </div>
+                              </div>
+                        </div>
+                        <div className="flex flex-wrap -mx-4">
+                              <div className="w-full md:w-1/2 lg:w-1/3 px-4">
+                                    <div className="max-w-[370px] mx-auto mb-10">
+                                          <div className="rounded overflow-hidden mb-8">
+                                                <img
+                                                      src="https://i.ibb.co/J2VXj7c/pexels-run-ffwpu-2403753.jpg"
+                                                      alt="image"
+                                                      className="w-full"
+                                                />
+                                          </div>
+                                          <div>
+                                                <span
+                                                      className="
+                     bg-[#f15b43]
+                     rounded
+                     inline-block
+                     text-center
+                     py-1
+                     px-4
+                     text-xs
+                     leading-loose
+                     font-semibold
+                     text-white
+                     mb-5
+                     "
+                                                >
+                                                      Dec 22, 2023
+                                                </span>
+                                                <h3>
+                                                      <a
+                                                            href="javascript:void(0)"
+                                                            className="
+                        font-semibold
+                        text-xl
+                        sm:text-2xl
+                        lg:text-xl
+                        xl:text-2xl
+                        mb-4
+                        inline-block
+                        text-dark
+                       hover:text-[#65c9bb]
+                        "
+                                                      >
+                                                            Fitness Fusion <br /> Fest 2021
+                                                      </a>
+                                                </h3>
+                                                <p className="text-base text-body-color">
+                                                      This high-energy event is all about making fitness
+                                                      fun and accessible to everyone.
+                                                </p>
+                                          </div>
+                                    </div>
+                              </div>
+                              <div className="w-full md:w-1/2 lg:w-1/3 px-4">
+                                    <div className="max-w-[370px] mx-auto mb-10">
+                                          <div className="rounded overflow-hidden mb-8">
+                                                <img
+                                                      src="https://i.ibb.co/yqzMnNx/propel-festival-second-2018.jpg"
+                                                      alt="image"
+                                                      className="w-full"
+                                                />
+                                          </div>
+                                          <div>
+                                                <span
+                                                      className="
+                     bg-[#f15b43]
+                     rounded
+                     inline-block
+                     text-center
+                     py-1
+                     px-4
+                     text-xs
+                     leading-loose
+                     font-semibold
+                     text-white
+                     mb-5
+                     "
+                                                >
+                                                      Mar 15, 2023
+                                                </span>
+                                                <h3>
+                                                      <a
+                                                            href="javascript:void(0)"
+                                                            className="
+                        font-semibold
+                        text-xl
+                        sm:text-2xl
+                        lg:text-xl
+                        xl:text-2xl
+                        mb-4
+                        inline-block
+                        text-dark
+                       hover:text-[#65c9bb]
+                        "
+                                                      >
+                                                            Mindful Living Symposium: Cultivating Inner Peace
+                                                      </a>
+                                                </h3>
+                                                <p className="text-base text-body-color">
+                                                      Dive into the world of mindfulness and inner peace at the Mindful Living Symposium.
+                                                </p>
+                                          </div>
+                                    </div>
+                              </div>
+                              <div className="w-full md:w-1/2 lg:w-1/3 px-4">
+                                    <div className="max-w-[370px] mx-auto mb-10">
+                                          <div className="rounded overflow-hidden mb-8">
+                                                <img
+                                                      src="https://i.ibb.co/9NhhSWP/lead-2.jpg"
+                                                      alt="image"
+                                                      className="w-full"
+                                                />
+                                          </div>
+                                          <div>
+                                                <span
+                                                      className="
+                     bg-[#f15b43]
+                     rounded
+                     inline-block
+                     text-center
+                     py-1
+                     px-4
+                     text-xs
+                     leading-loose
+                     font-semibold
+                     text-white
+                     mb-5
+                     "
+                                                >
+                                                      Jan 05, 2023
+                                                </span>
+                                                <h3>
+                                                      <a
+                                                            href="javascript:void(0)"
+                                                            className="
+                        font-semibold
+                        text-xl
+                        sm:text-2xl
+                        lg:text-xl
+                        xl:text-2xl
+                        mb-4
+                        inline-block
+                        text-dark
+                       hover:text-[#65c9bb]
+                        "
+                                                      >
+                                                            Wellness Expo 2023: Nourish Your Mind and Body
+                                                      </a>
+                                                </h3>
+                                                <p className="text-base text-body-color">
+                                                      Wellness Expo 2023, a comprehensive health and wellness event designed to help you achieve optimal well-being.
+                                                </p>
+                                          </div>
+                                    </div>
+                              </div>
+                        </div>
+                  </div>
+            </section>
+      );
+};
+
+export default RecentEvent;
