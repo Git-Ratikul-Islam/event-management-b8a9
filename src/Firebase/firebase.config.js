5b622da0b63de47e268ad8a652c611622cda0679
Ratikulislam <joyofficial80@gmail.com>
2023-12-16 13:15:03 +0600

    created-contact-us-message-feature-using-emailjs

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -8,15 +8,23 @@ import { getAuth } from "firebase/auth";
 console.log(import.meta.env.VITE_PASS);
 
 const firebaseConfig = {
-      apiKey: import.meta.env.VITE_APIKEY,
-      authDomain: import.meta.env.VITE_AUTHDOMAIN,
-      projectId: import.meta.env.VITE_PROJECTID,
-      storageBucket: import.meta.env.VITE_STORAGEBUCKET,
-      messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
-      appId: import.meta.env.VITE_APPID
+      apiKey: "AIzaSyAiLpQ4ZCJTqbMwmvAj5MK2nE1-e8-ysZ4",
+      authDomain: "wholehealth-1fcbe.firebaseapp.com",
+      projectId: "wholehealth-1fcbe",
+      storageBucket: "wholehealth-1fcbe.appspot.com",
+      messagingSenderId: "1018841568352",
+      appId: "1:1018841568352:web:04074a6e0714440c0d9f05"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 export default auth;
+
+
+// apiKey: import.meta.env.VITE_APIKEY,
+// authDomain: import.meta.env.VITE_AUTHDOMAIN,
+// projectId: import.meta.env.VITE_PROJECTID,
+// storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+// messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+// appId: import.meta.env.VITE_APPID
